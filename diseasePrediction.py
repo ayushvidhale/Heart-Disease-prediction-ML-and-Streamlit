9115b6e13823877fb46fc07b0e8b0ec885fb53a7
ayushvidhale <ayushvidhale099@gmail.com>
2024-02-24 15:17:38 +0530

    docs update

--- diseasePrediction.py
+++ diseasePrediction.py
@@ -10,7 +10,7 @@ heart_disease_model = pickle.load(open('saved models/heart_disease_model.sav', '
 # st.header('Disease Prediction System')
 
 # page title
-st.title('Health Prediction using ML')
+st.title('Early Detection of Diabetes and Heart Disease using Random Forest')
     
 # getting the input data from the user
 col1, col2, col3 = st.columns(3)
@@ -110,13 +110,6 @@ thal = 0.0
 input_data = [[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]]
 
 
-
-#intialize ecg object
-ecg = ECG()
-#get the uploaded image
-uploaded_file = st.file_uploader("Choose a file")
-
-
     # creating a button for Prediction
 if st.button('Predict Health Result'):
     heart_prediction = heart_disease_model.predict(input_data)[0]
@@ -152,68 +145,3 @@ if st.button('Predict Health Result'):
 st.success(f'Heart Health Score: {prediction_probability:.2f}')
 st.success(heart_diagnosis)
 st.success(diab_diagnosis)
-
-
-if uploaded_file is not None:
-  """#### **UPLOADED IMAGE**"""
-  # call the getimage method
-  ecg_user_image_read = ecg.getImage(uploaded_file)
-  #show the image
-  st.image(ecg_user_image_read)
-
-  """#### **GRAY SCALE IMAGE**"""
-  #call the convert Grayscale image method
-  ecg_user_gray_image_read = ecg.GrayImgae(ecg_user_image_read)
-  
-  #create Streamlit Expander for Gray Scale
-  my_expander = st.expander(label='Gray SCALE IMAGE')
-  with my_expander: 
-    st.image(ecg_user_gray_image_read)
-  
-  """#### **DIVIDING LEADS**"""
-   #call the Divide leads method
-  dividing_leads=ecg.DividingLeads(ecg_user_image_read)
-
-  #streamlit expander for dividing leads
-  my_expander1 = st.expander(label='DIVIDING LEAD')
-  with my_expander1:
-    st.image('Leads_1-12_figure.png')
-    st.image('Long_Lead_13_figure.png')
-  
-  """#### **PREPROCESSED LEADS**"""
-  #call the preprocessed leads method
-  ecg_preprocessed_leads = ecg.PreprocessingLeads(dividing_leads)
-
-  #streamlit expander for preprocessed leads
-  my_expander2 = st.expander(label='PREPROCESSED LEAD')
-  with my_expander2:
-    st.image('Preprossed_Leads_1-12_figure.png')
-    st.image('Preprossed_Leads_13_figure.png')
-  
-  """#### **EXTRACTING SIGNALS(1-12)**"""
-  #call the sognal extraction method
-  ec_signal_extraction = ecg.SignalExtraction_Scaling(dividing_leads)
-  my_expander3 = st.expander(label='CONOTUR LEADS')
-  with my_expander3:
-    st.image('Contour_Leads_1-12_figure.png')
-  
-  """#### **CONVERTING TO 1D SIGNAL**"""
-  #call the combine and conver to 1D signal method
-  ecg_1dsignal = ecg.CombineConvert1Dsignal()
-  my_expander4 = st.expander(label='1D Signals')
-  with my_expander4:
-    st.write(ecg_1dsignal)
-    
-  """#### **PERFORM DIMENSINALITY REDUCTION**"""
-  #call the dimensinality reduction funciton
-  ecg_final = ecg.DimensionalReduciton(ecg_1dsignal)
-  my_expander4 = st.expander(label='Dimensional Reduction')
-  with my_expander4:
-    st.write(ecg_final)
-  
-#   """#### **PASS TO PRETRAINED ML MODEL FOR PREDICTION**"""
-# #   call the Pretrainsed ML model for prediction
-#   ecg_model=ecg.ModelLoad_predict(ecg_final)
-#   my_expander5 = st.expander(label='PREDICTION')
-#   with my_expander5:
-#     st.write(ecg_model)
